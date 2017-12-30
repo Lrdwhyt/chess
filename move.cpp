19a1630a6e8c73a34c8c5e15c8c10d39828dae35
曹己侯 <lrdwhyt@gmail.com>
2017-12-29 18:00:08 -0800

    add move validation helper functions to Move class

--- move.cpp
+++ move.cpp
@@ -21,3 +21,116 @@ Move Move::fromString(std::string str) {
     }
     return Move(originSquare, destSquare);
 }
+
+bool Move::isRookMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (x == 0 || y == 0) {
+        // This includes cases where both x and y are 0, i.e. origin == destination
+        // That is checked elsewhere to reduce redundancy
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Move::isBishopMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (abs(x) == abs(y)) {
+        // This includes cases where both x and y are 0, i.e. origin == destination
+        // That is checked elsewhere to reduce redundancy
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Move::isQueenMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (x == 0 || y == 0 || abs(x) == abs(y)) {
+        // This includes cases where both x and y are 0, i.e. origin == destination
+        // That is checked elsewhere to reduce redundancy
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Move::isKnightMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if ((abs(x) == 2 && abs(y) == 1) || (abs(x) == 1 && abs(y) == 2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Move::isKingMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (abs(x) <= 1 && abs(y) <= 1) {
+        // This includes cases where both x and y are 0, i.e. origin == destination
+        // That is checked elsewhere to reduce redundancy
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Move::isCastleMove() {
+    if (origin == Square::get(Column::E, 1)) {
+        if (destination == Square::get(Column::C, 1) || destination == Square::get(Column::G, 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (origin == Square::get(Column::E, 8)) {
+        if (destination == Square::get(Column::C, 8) || destination == Square::get(Column::G, 8)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+bool Move::isPawnMove() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (x != 0) {
+        return false;
+    }
+
+    if (abs(y) == 1) {
+        return true; // Move forward/backwards one space
+    } else if (y == 2) {
+        if (Square::getRow(origin) == 2) {
+            // Pawns have to be in original position to be moved up twice
+            return true; // White move forward two spaces
+        } else {
+            return false;
+        }
+    } else if (y == -2) {
+        if (Square::getRow(origin) == 7) {
+            return true; // Black move forward two spaces
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+bool Move::isPawnCapture() {
+    int x, y;
+    std::tie(x, y) = Square::diff(origin, destination);
+    if (abs(x) == 1 && abs(y) == 1) {
+        return true; // Pawn capture
+    } else {
+        return false;
+    }
+}
--- move.h
+++ move.h
@@ -18,6 +18,20 @@ public:
     Move();
     Move(int, int);
     static Move fromString(std::string);
+
+    /*
+     * The following functions are used for move validation
+     * They evaluate whether a certain move /could/ be a legal move of that piece,
+     * not whether it actually is one or not
+     */
+    bool isPawnMove();
+    bool isPawnCapture();
+    bool isBishopMove();
+    bool isKnightMove();
+    bool isRookMove();
+    bool isQueenMove();
+    bool isKingMove();
+    bool isCastleMove();
 };
 
 #endif
