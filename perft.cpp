a905b36bf56083368a2c83ccb8c1b7d768909b99
unknown <lrdwhyt@gmail.com>
2021-07-14 00:38:41 +0900

    refactor perft test to use PerftTestCase struct to reduce code repetition

--- perft.cpp
+++ perft.cpp
@@ -28,29 +28,28 @@ std::vector<std::tuple<Move, long long>> Perft::divide(GameState const &gamestat
         GameState branch = GameState(gamestate);
         branch.processMove(move);
         long long total = perft(branch, depth - 1);
-        results.emplace_back(std::make_tuple(move, total));
+        results.push_back(std::make_tuple(move, total));
     }
     return results;
 }
 
+struct PerftTestCase {
+    std::string fenString;
+    int depth;
+    long long result;
+};
+
 void Perft::test() {
-    GameState testState1 = GameState("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
-    const int testResult1 = perft(testState1, 4);
-    assert(testResult1 == 4085603);
+    std::vector<PerftTestCase> testcases;
+    testcases.push_back({ "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 4, 4085603 });
+    testcases.push_back({ "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3894594 });
+    testcases.push_back({ "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 6, 11030083 });
+    testcases.push_back({ "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 5, 15833292 });
+    testcases.push_back({ "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 5, 89941194 });
 
-    GameState testState2 = GameState("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
-    const int testResult2 = perft(testState2, 4);
-    assert(testResult2 == 3894594);
-
-    GameState testState3 = GameState("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-    const int testResult3 = perft(testState3, 6);
-    assert(testResult3 == 11030083);
-
-    GameState testState4 = GameState("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    const int testResult4 = perft(testState4, 5);
-    assert(testResult4 == 15833292);
-
-    GameState testState5 = GameState("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
-    const int testResult5 = perft(testState5, 5);
-    assert(testResult5 == 89941194);
+    for (PerftTestCase const &testcase : testcases) {
+        GameState testState = GameState(testcase.fenString);
+        const long long result = perft(testState, testcase.depth);
+        assert(result == testcase.result);
+    }
 }
